8216737d2c40f52fb77af5b52ffcfa9ebd2deb60
Tibi Halasi <czalbert.tibor04@gmail.com>
2023-09-21 11:15:47 +0200

    Filmcím részlet keresés

--- FilmProject_VerzioKezelt/Program.cs
+++ FilmProject_VerzioKezelt/Program.cs
@@ -22,8 +22,88 @@ namespace FilmProject_VerzioKezelt
                 Debug.WriteLine(item.Cim);
             }
             Feladat01();
+            Feladat02();
+            Feladat03();
+            Feladat04();
 
             Console.ReadKey();
+        }
+
+        private static void Feladat04()
+        {
+            Console.WriteLine("Adja meg a keresendő tartalmat:");
+            string keresendo = Console.ReadLine();
+            List<string> cimek = FilmReszKeres(keresendo);
+            Console.WriteLine("A keresett részlet az alábbi filmek címében szerepel: ");
+            foreach (var item in cimek)
+            {
+                Console.WriteLine("\t" + item);
+            }
+        }
+
+        private static List<string> FilmReszKeres(string keresendo)
+        {
+            List<string> cimek = new List<string>();
+            foreach (var film in filmek)
+            {
+                if (film.Cim.ToLower().Contains(keresendo.ToLower()))
+                {
+                    cimek.Add(film.Cim);
+                }
+            }
+            return cimek;
+        }
+
+        private static void Feladat03()
+        {
+            Console.WriteLine("Adjon meg egy film címet:");
+            string cim = Console.ReadLine();
+            Film film = FilmKeres(cim);
+            if (film == null)
+            {
+                Console.WriteLine("A megadott film nem található");
+            }
+            else
+            {
+                Console.WriteLine($"A megadott film {film.Hossz} perces");
+            }
+        }
+
+        private static Film FilmKeres(string cim)
+        {
+            int i = 0;
+            while (i < filmek.Count && filmek[i].Cim != cim)
+            {
+                i++;
+            }
+            if (i < filmek.Count)
+            {
+                return filmek[i];
+            }
+            else
+            { 
+                return null; 
+            }
+        }
+
+        private static void Feladat02()
+        {
+            Film leghosszabbFilm = GetLeghosszabbFilm();
+
+            Console.WriteLine($"2. A leghosszabb film: {leghosszabbFilm.Cim} - {leghosszabbFilm.Hossz} perc");
+        }
+
+        private static Film GetLeghosszabbFilm()
+        {
+            Film leghosszabbFilm = filmek[0];
+            for (int i = 1; i < filmek.Count; i++)
+            {
+                if (filmek[i].Hossz > leghosszabbFilm.Hossz)
+                {
+                    leghosszabbFilm = filmek[i];
+                }
+            }
+            return leghosszabbFilm;
         }
 
         private static void Feladat01()
